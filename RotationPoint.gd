6bf1c61801ffb2678335d64f072c220152320079
Leon Stumpf <stumpfleon@gmx.de>
2024-04-26 12:13:48 +0200

    schwerter können variabel erschaffen werden

--- RotationPoint.gd
+++ RotationPoint.gd
@@ -7,20 +7,12 @@ var angular_speed = PI * speed
 var score = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sword.position = offset(1)
+	$Sword.position = Vector2(0, -(sword_offset))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	spin(delta)
-
-func offset(number):
-	var offset = Vector2(0, sword_offset).rotated((2*PI)/number)
-	return offset
 	
+
 func spin(delta):
 	rotation += angular_speed * delta
-	
-func duplicate_sword(count):
-	pass
-	
-	
